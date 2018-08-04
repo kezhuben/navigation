c77344500babc2cadc7ec7422d77377a6b6512eb
kezhuben <540967300@qq.com>
2018-08-04 15:00:08 +0800

    commit

--- __init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-# author: yangcheng
-# license: © 2011-2017 The Authors. All Rights Reserved.
-# contact: yangcheng@zuzuche.com
-# time: 2017/11/14 15:26
-# desc:
-# ======================================================
--- nets_params/faster_rcc_params.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-# author: yangcheng
-# license: © 2011-2017 The Authors. All Rights Reserved.
-# contact: yangcheng@zuzuche.com
-# time: 2017/10/24 14:27
-# desc: The default parameters for faster rcnn, algorithm details see: ../nets/faster_rcnn.py
-# ======================================================
-
